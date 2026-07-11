265453f7bf354061c8cf87c756e150c8fbd474ce
juyterman1000 <fastrunner10090@gmail.com>
2026-07-11 18:48:50 +0000

    Release v1.0.47: restore stable SimHash geometry

--- entroly/native_status.py
+++ entroly/native_status.py
@@ -7,7 +7,7 @@ from importlib import metadata
 from types import ModuleType
 
 
-MIN_ENTROLY_CORE_VERSION = "1.0.46"
+MIN_ENTROLY_CORE_VERSION = "1.0.47"
 QCCR_SYMBOLS = (
     "py_qccr_expand_query",
     "py_qccr_rank_files",
