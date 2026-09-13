9bab5850955cff6f1e374fada7227d45c9f2f570
juyterman1000 <208309368+juyterman1000@users.noreply.github.com>
2026-09-13 17:43:32 +0000

    Update Homebrew formula for 1.0.84

--- tests/test_release_surface.py
+++ tests/test_release_surface.py
@@ -14,12 +14,12 @@ from pyproject_compat import read_project_metadata
 
 ROOT = Path(__file__).resolve().parents[1]
 RELEASE_VERSION = "1.0.84"
-HOMEBREW_FORMULA_VERSION = "1.0.81"
+HOMEBREW_FORMULA_VERSION = "1.0.84"
 HOMEBREW_FORMULA_URL = (
-    "https://files.pythonhosted.org/packages/8b/90/fdd30ed5bbe7586979259de173"
-    "b9d76668bab086a4c2589b65d26946134c/entroly-1.0.81.tar.gz"
+    "https://files.pythonhosted.org/packages/17/b6/7901325dc87c9c2088fbc622a7"
+    "b2d8330f6d8866d27c5e7efe7c10639f03/entroly-1.0.84.tar.gz"
 )
-HOMEBREW_FORMULA_SHA256 = "e419dff573c04e7e4a9ce5ee74d499964ee743fbb9d67b955a0883787ac33689"
+HOMEBREW_FORMULA_SHA256 = "0033c5c52e1a733f148e9d7b5abf953a064ea1fb86b31f2e028205ca3fdd9817"
 CANONICAL_MCP_NAME = "io.github.juyterman1000/entroly"
 CANONICAL_REPOSITORY = "https://github.com/juyterman1000/entroly"
 
