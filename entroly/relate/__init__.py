eb8d7e09a4e03f6c222a1e7d284f60d99cb98ad8
Jupyterman <fastrunner10090@gmail.com>
2026-09-18 00:45:21 -0700

    research(relate-x): expose research primitives

---
+++ entroly/relate/__init__.py
@@ -0,0 +1,35 @@
+"""RELATE-X research primitives.
+
+Experimental only: these modules do not wire into Entroly production paths.
+"""
+from .types import (
+    AuthorityEnvelope,
+    ConstraintSpan,
+    QueryContract,
+    RelationVector,
+    EvidenceCandidate,
+    CollisionReport,
+    DifferentialSpan,
+    OmissionWitness,
+)
+from .constraints import compile_query_contract
+from .collision import detect_semantic_collision
+from .differential import extract_differential_spans
+from .actions import normalize_action
+from .omission import verify_omission_safety
+
+__all__ = [
+    "AuthorityEnvelope",
+    "ConstraintSpan",
+    "QueryContract",
+    "RelationVector",
+    "EvidenceCandidate",
+    "CollisionReport",
+    "DifferentialSpan",
+    "OmissionWitness",
+    "compile_query_contract",
+    "detect_semantic_collision",
+    "extract_differential_spans",
+    "normalize_action",
+    "verify_omission_safety",
+]
